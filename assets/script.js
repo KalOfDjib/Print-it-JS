d09f1f3da795764ead2514043835cf6f627f292e
Abdo MALAKAN <mknado@gmail.com>
2023-09-06 13:38:31 +0200

    action fleche gauche ok à par pour SPAN

--- assets/script.js
+++ assets/script.js
@@ -17,15 +17,63 @@ const slides = [
 	}
 ]
 
+/*let abcd = document.querySelectorAll(".dots dot_selected");
+console.log("Vous avez cliqué sur le bouton gauche");
+let currentImageNumberInt = parseInt(currentImageNumber);*/
+
+
+
 let fleche_gauche = document.getElementById("fleche_gauche");
 fleche_gauche.addEventListener("click", function () 
-{
-console.log("Vous avez cliqué sur le bouton gauche")
-});
+	{
+	let activeDiv = document.querySelectorAll(".dots .dot_selected");
+	let currentImageNumberId=activeDiv[0].id
+	if (currentImageNumberId>0)
+		{
+		NewCurrentImageNumberId=currentImageNumberId-1
+		}
+		else {
+		NewCurrentImageNumberId=3
+		}
+	console.log(NewCurrentImageNumberId)
+
+	let RetirerDotSelected = document.getElementById(currentImageNumberId)
+	console.log(RetirerDotSelected)
+	RetirerDotSelected.classList.remove("dot_selected")
+	let AjoutDotSelected = document.getElementById(NewCurrentImageNumberId)
+	AjoutDotSelected.classList.add("dot_selected")
+	let ChangementImage = document.getElementById("banner-img")
+	console.log(ChangementImage)
+	ChangementImage.src = "./assets/images/slideshow/" + slides[NewCurrentImageNumberId].image
+	let changementCommentaires = document.querySelector("#paragraphe");
+	console.log(changementCommentaires)
+	changementCommentaires.textContent = slides[NewCurrentImageNumberId].tagLine
+	}
+);
 
 let fleche_droite = document.getElementById("fleche_droite");
 fleche_droite.addEventListener("click", function () 
 {
-console.log("Vous avez cliqué sur le bouton droit")
-console.log(slides)
+	let activeDiv = document.querySelectorAll(".dots .dot_selected");
+	let currentImageNumberId=activeDiv[0].id
+	if (currentImageNumberId>0)
+		{
+		NewCurrentImageNumberId=currentImageNumberId-1
+		}
+		else {
+		NewCurrentImageNumberId=3
+		}
+	console.log(NewCurrentImageNumberId)
+
+	let RetirerDotSelected = document.getElementById(currentImageNumberId)
+	console.log(RetirerDotSelected)
+	RetirerDotSelected.classList.remove("dot_selected")
+	let AjoutDotSelected = document.getElementById(NewCurrentImageNumberId)
+	AjoutDotSelected.classList.add("dot_selected")
+	let ChangementImage = document.getElementById("banner-img")
+	console.log(ChangementImage)
+	ChangementImage.src = "./assets/images/slideshow/" + slides[NewCurrentImageNumberId].image
+	let changementCommentaires = document.querySelector("#paragraphe");
+	console.log(changementCommentaires)
+	changementCommentaires.textContent = slides[NewCurrentImageNumberId].tagLine
 });
